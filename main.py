2f50d01707acfdaccfcc432e71801493a0ca097d
SadmanTariq <sadmantariq@gmail.com>
2019-09-12 20:34:47 +0600

    Exception to bot's own messages

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ except FileNotFoundError:
 client = discord.Client()
 
 authorized_user_ids = [340115550208262145]
+bot_user_id = 593359969806712861
 responses = {
     "fuck u": ["no, fuck you",
                "fuck you harder",
@@ -27,6 +28,9 @@ responses = {
 
 @client.event
 async def on_message(message):
+    if message.author.id == bot_user_id:
+        return
+
     print(message.author.id)
     if message.content == "!!shutdown":
         if message.author.id in authorized_user_ids:
@@ -37,7 +41,7 @@ async def on_message(message):
 
     for trigger, response_list in responses.items():
         if message.content.lower().find(trigger) != -1:
-            response = response_list[randint(len(response_list))]
+            response = response_list[randint(0, len(response_list) - 1)]
             response = response.format(message.author.name)
             await message.channel.send(response)
             return
